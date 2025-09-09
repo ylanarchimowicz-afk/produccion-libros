70333420801a76e9ba44e02dad9cec6f58128a88
Ylan (work) <tu-email@empresa.com>
2025-09-09 19:06:37 -0300

    build(ui): Tailwind v4 correcto (@tailwindcss/postcss), importar index.css y scripts Vite

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-﻿import tailwind from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
-
-export default {
-  plugins: [tailwind(), autoprefixer()],
-};
+﻿export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    "autoprefixer": {}
+  }
+}
