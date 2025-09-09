0503ecece85be88c8eac83d9d56532f4396d4551
Ylan (work) <tu-email@empresa.com>
2025-09-09 18:51:31 -0300

    style: UI global decente (ui.css) + import en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
-﻿import "./hotfix.css";
+﻿import "./ui.css";
+import "./hotfix.css";
 const root = document.getElementById("root");
 root.innerHTML = `
   <div style="font-family:ui-sans-serif;line-height:1.3">
@@ -7,3 +8,4 @@ root.innerHTML = `
     <div class="machine-card"><h3>Ricoh 8400 (ByN)</h3><p>Setup 3 min  Cierre 0 min</p></div>
   </div>
 `;
+
