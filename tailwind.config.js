8aea40da861690eb452dacad5138fcc956114c4e
Ylan (work) <tu-email@empresa.com>
2025-09-09 15:49:52 -0300

    fix(tw): ampliar content a raíz y src para que aplique estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 ﻿/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./*.{js,jsx,ts,tsx}",          // < archivos en la raíz (index.tsx, etc.)
+    "./src/**/*.{js,jsx,ts,tsx}"    // < todo lo de /src
+  ],
   theme: { extend: {} },
   plugins: [],
 };
