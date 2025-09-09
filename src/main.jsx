96154436cc5b9c256e61ce5bd8eb003a50940bad
Ylan (work) <tu-email@empresa.com>
2025-09-09 15:33:17 -0300

    build(ui): Tailwind + estilos (cards/grid/header)

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+﻿import './index.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -8,3 +9,4 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
